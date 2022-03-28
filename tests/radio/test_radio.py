844eb9b4c1b3c7e210cda908f8a7a96da105a426
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-03-28 14:03:16 -0300

    add tests for radio-button elements

---
+++ tests/radio/test_radio.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from src.webelement import element
+
+
+class TestRadio:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, driver: WebDriver):
+        self.driver = driver
+        self.driver.get("https://demoqa.com/radio-button")
+
+        # elements
+        self.yes_radio_button = element(self.driver, (By.CSS_SELECTOR, "label[for='yesRadio']"))
+        self.impressive_radio_button = element(
+            self.driver, (By.CSS_SELECTOR, "label[for='impressiveRadio']")
+        )
+        self.no_radio_button = element(self.driver, (By.CSS_SELECTOR, "#noRadio"))
+        self.output = element(self.driver, (By.CSS_SELECTOR, ".mt-3"))
+
+    def test_select_yes(self):
+        self.yes_radio_button.click()
+        assert self.output.contain_text("You have selected Yes")
+
+    def test_select_impressive(self):
+        self.impressive_radio_button.click()
+        assert self.output.contain_text("You have selected Impressive")
+
+    def test_no_radio_button_is_disabled(self):
+        assert self.no_radio_button.is_enabled() is False
